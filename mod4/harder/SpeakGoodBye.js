4b9f0e886d25bb6d7fc7a0706bd91012ba7e349b
sam <samsonchengys@gmail.com>
2023-05-27 21:37:40 +0800

    mod4 harder folder

--- mod4/harder/SpeakGoodBye.js
+++ mod4/harder/SpeakGoodBye.js
@@ -3,25 +3,6 @@
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
-(function (window) {
-
-var byeSpeaker = {};
-byeSpeaker.speakWord = "Good Bye";
-window.byeSpeaker = byeSpeaker;
-
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
-  if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
-  } else {
-    helloSpeaker.speak(names[i]);
-  }
-}
- byeSpeaker.speak = function(name) {
-  console.log(byeSpeaker.speakWord + " " + name);
-}
-})(window);
 
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
@@ -30,11 +11,14 @@ for (var i = 0; i < names.length; i++) {
 // var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
+var speakWord = "Good Bye";
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
--- mod4/harder/SpeakHello.js
+++ mod4/harder/SpeakHello.js
@@ -1,26 +1,5 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
-(function (window) {
-
-var helloSpeaker = {};
-helloSpeaker.speakWord = "Hello";
-window.helloSpeaker = helloSpeaker;
-
-// var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-// for (var i = 0; i < names.length; i++) {
-//   var firstLetter = names[i].charAt(0).toLowerCase();
-//   if (firstLetter === 'j') {
-//     byeSpeaker.speak(names[i]);
-//   } else {
-//     helloSpeaker.speak(names[i]);
-//   }
-// }
-
- helloSpeaker.speak = function(name) {
-  console.log(helloSpeaker.speakWord + " " + name);
-}
-
-})(window);
 
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
@@ -29,10 +8,14 @@ window.helloSpeaker = helloSpeaker;
 // var helloSpeaker =
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
+var speakWord = "Hello";
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
 
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
