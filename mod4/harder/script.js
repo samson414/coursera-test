279a2b5b5727fdea25a28fee65ddbb3203064155
sam <samsonchengys@gmail.com>
2023-06-11 22:25:33 +0800

    removed unnecessary folders and added mod3/mod4 html/css folders

--- mod4/harder/script.js
+++ mod4/harder/script.js
@@ -40,8 +40,8 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
-
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt().toLowerCase(); 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
@@ -56,9 +56,9 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i])
   } else {
-    // helloSpeaker.xxxx
+    helloSpeaker.speak(names[i])
   }
 }
